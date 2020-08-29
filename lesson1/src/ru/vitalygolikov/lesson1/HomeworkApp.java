b627ef0499c9ca3f7a778c600673c7487c2792c8
Vitaly Golikov <vitalygolikov09@gmail.com>
2020-08-29 12:16:33 +0300

    Homework: lesson1

---
+++ lesson1/src/ru/vitalygolikov/lesson1/HomeworkApp.java
@@ -0,0 +1,168 @@
+package ru.vitalygolikov.lesson1;
+
+public class HomeworkApp {
+    public static void main(String args[])
+    {
+        /*
+        * Задание 2:
+        * Cоздать и инициализировать переменные каждого из 8 примитивных типов
+        */
+
+        byte bVar = 12;
+        short sVar = 500;
+        int iVar = 100000;
+        long lVar = 512l;
+
+        float fVar = 1.5f;
+        double dVar = 8.53;
+
+        char cVar = 's';
+        boolean blVar = true;
+
+
+        /*
+        * Задание 3
+        * Вычисление выражения
+        */
+        float a = 1.23f;
+        float b = 5.67f;
+        float c = 45.f;
+        float d = 437.78f;
+
+        float exThreeResult = exerciseThree( a, b, c, d);
+        System.out.println("Результат задания № 3 равен " + exThreeResult);
+
+        /*
+        * Задание 4
+        * Метод возвращает true, если число лежит в интервале [10, 20],
+        * иначе false
+        */
+        float number1 = 8.5f;
+        float number2 = 5.6f;
+
+        boolean exFourResult = exerciseFour( number1, number2 );
+        System.out.println("Результат выполнения задания №4 равен " + exFourResult);
+
+        /*
+        * Задание 5
+        * Печатает, положительное или отрицательное число передано в качестве аргумента
+        */
+        int number = -8;
+        System.out.print("Результат выполнения задания №5: ");
+        numberSign(number);
+
+        /*
+        * Задание 6
+        * Если в метод передано положительное число, возвращается true, иначе false
+        */
+        number = 7;
+        boolean exResultSix = isPositive(number);
+        System.out.println("Результат выполнения задания № 6 равен " + exResultSix);
+
+        /*
+        * Задание 7
+        * Метод принимает строку и выводит сообщение "Привет, [аргумент]"
+        */
+        helloAnyone("Антон");
+        helloAnyone("Юзер");
+
+        /*
+        * Задание 8
+        * Метод определяет и печатает в консоль, является ли год високосным
+        */
+
+        isLeapYear(345);
+        isLeapYear(2357);
+        isLeapYear(160000);
+        isLeapYear(3000);
+    }
+
+    //Задание 3
+    static float exerciseThree(float a, float b, float c, float d)
+    {
+        return a * ( b + (c/d) );
+    }
+
+    //Задание 4
+    static boolean exerciseFour(float number1, float number2)
+    {
+        boolean result = false;
+        float sum = number1 + number2;
+        if( sum >= 10.f )
+        {
+            if( sum <= 20.f)
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    //Задание 5
+    static void numberSign( int number )
+    {
+        if(number >= 0)
+        {
+            System.out.println("Число " + number + " положительное");
+        }
+        else
+        {
+            System.out.println("Число " + number + " отрицательное");
+        }
+    }
+
+    //Задание 6
+    static boolean isPositive(int number)
+    {
+        boolean result;
+
+        if(number >= 0)
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+        return result;
+    }
+
+    //Задание 7
+    static void helloAnyone(String name)
+    {
+        System.out.println("Привет, " + name);
+    }
+
+    //Задание 8
+    static void isLeapYear(int year)
+    {
+        boolean isLeap;
+        if( year % 4 == 0 )
+        {
+            isLeap = true;
+
+            if(year % 100 == 0)
+            {
+                if( year % 400 != 0)
+                {
+                    isLeap = false;
+                }
+            }
+
+        }
+        else
+        {
+            isLeap = false;
+        }
+
+        if(isLeap)
+        {
+            System.out.println("Год " + year + " високосный");
+        }
+        else
+        {
+            System.out.println("Год " + year + " не високосный");
+        }
+
+    }
+}
